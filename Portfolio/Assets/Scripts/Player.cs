1c9b4fcc6649059f8788039f8f1b1475d55e4e56
LeeHW-Git-new <71817406+LeeHW-Git-new@users.noreply.github.com>
2021-07-28 21:51:21 +0900

    -2021 07 28
    Framework
    캐릭터 앉기 달리기 점프 구현

--- Portfolio/Assets/Scripts/Player.cs
+++ Portfolio/Assets/Scripts/Player.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //움직임 스피드 조정 관련
     [SerializeField] //이진화
     private float MoveSpeed;
+    [SerializeField]
+    private float RunSpeed;
+    [SerializeField]
+    private float crouchSpeed;
 
+    private float applySpeed;
 
+    [SerializeField]
+    private float JumpForce;
+
+    //상태 변수
+    private bool isRun = false;
+    private bool isGround = true;
+    private bool isCrouch = false;
+
+    // 앉았을 때 얼마나 앉을지 결정하는 변수.
+    [SerializeField]
+    private float crouchPosY;
+    private float originPosY;
+    private float applyCrouchPosY;
+
+    //땅 착지
+    private CapsuleCollider capsuleCollider;
+
+    //카메라 민감도
     [SerializeField]
     private float LookSensitivity;
 
@@ -22,15 +46,24 @@ public class Player : MonoBehaviour
 
     void Start()
     {
+        capsuleCollider = GetComponent<CapsuleCollider>();
         myRigid = GetComponent<Rigidbody>();
-    }
+        applySpeed = MoveSpeed;
+        originPosY = theCamera.transform.localPosition.y;
+        applyCrouchPosY = originPosY;
 
+    }
 
     void Update()
     {
-        Move();
+        IsGround();
+        TryJump();
+        TryRun();
+        TryCrouch();
+        Move(); 
         CameraRotation();
         CharacterRotation();
+        
     }
     private void Move()
     {
@@ -40,7 +73,7 @@ public class Player : MonoBehaviour
         Vector3 moveHorizontal = transform.right * moveDirX;
         Vector3 moveVertical = transform.forward * moveDirZ;
 
-        Vector3 velcoity = (moveHorizontal + moveVertical).normalized * MoveSpeed;
+        Vector3 velcoity = (moveHorizontal + moveVertical).normalized * applySpeed;
 
         myRigid.MovePosition(transform.position + velcoity * Time.deltaTime);
 
@@ -60,5 +93,94 @@ public class Player : MonoBehaviour
         float yRotation = Input.GetAxis("Mouse X");
         Vector3 CharacterRotationY = new Vector3(0f, yRotation, 0f) * LookSensitivity;
         myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(CharacterRotationY));
+    }
+    private void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+    private void Running()
+    {
+        isRun = true;
+        applySpeed = RunSpeed;
+    }
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = MoveSpeed;
+    }
+    private void TryJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        //앉은 상태에서 점프시 앉은상태 해제
+        if (isCrouch)
+            Crouch();
+
+        myRigid.velocity = transform.up * JumpForce;
+    }
+
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down,capsuleCollider.bounds.extents.y + 0.1f); // CapsuleCollider 반사이즈 만큼 아래로 + 여유분(EX 경사)
+    }
+
+    // 앉기 시도
+    private void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+    // 앉기 동작
+    private void Crouch()
+    {
+        isCrouch = !isCrouch;
+
+        if (isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = MoveSpeed;
+            applyCrouchPosY = originPosY;
+        }
+
+        StartCoroutine(CrouchCoroutine());
+
+    }
+
+    //부드러운 앉기 동작 코루틴
+    IEnumerator CrouchCoroutine()
+    {
+        float _posY = theCamera.transform.localPosition.y;
+        int count = 0;
+
+        while (_posY != applyCrouchPosY)
+        {
+            count++;
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.3f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+            if (count > 15)
+                break;
+            yield return null;
+        }
+        theCamera.transform.localPosition = new Vector3(0, applyCrouchPosY, 0f);
     }
 }
